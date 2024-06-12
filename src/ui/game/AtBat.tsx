ef35321e6f5957afe4c3df20f7c65335ad75945c
Mitchell Christ <xzmaru@gmail.com>
2024-06-11 21:47:03 -0700

    add primaryPosition

--- src/ui/game/AtBat.tsx
+++ src/ui/game/AtBat.tsx
@@ -1,13 +1,24 @@
+import { liveMLB } from '@/lib/mlb'
 import Panel from '../Panel'
 
 export default function AtBat({ linescore }: { linescore: MLB.LiveLineScore }) {
+	const { data } = liveMLB<{ people: MLB.Player[] }>(
+		linescore.offense.batter.link,
+	)
+
+	const batter = data?.people?.[0]
+
 	return (
 		<div className="hidden gap-1 overflow-hidden whitespace-nowrap text-xs *:ml-auto *:flex *:gap-x-1 @xl:grid">
 			<div>
 				P: <Panel>{linescore.defense.pitcher.fullName}</Panel>
 			</div>
 			<div>
-				AB: <Panel>{linescore.offense.batter.fullName}</Panel>
+				{'AB: '}
+				<Panel>
+					{linescore.offense.batter.fullName}{' '}
+					{batter && <small>{batter.primaryPosition.abbreviation}</small>}
+				</Panel>
 			</div>
 		</div>
 	)
