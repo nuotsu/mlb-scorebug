d42f2c0391fb8d1891a12edbba4986394b60dbbe
Mitchell Christ <xzmaru@gmail.com>
2024-06-11 20:20:40 -0700

    fix sessionStorage

--- src/ui/game/HideScore.tsx
+++ src/ui/game/HideScore.tsx
@@ -1,21 +1,28 @@
 'use client'
 
+import { useEffect, useRef } from 'react'
 import { BiShow, BiHide } from 'react-icons/bi'
 
 export default function HideScore({ game }: { game: MLB.ScheduleGame }) {
+	const ref = useRef<HTMLInputElement>(null)
+
+	useEffect(() => {
+		if (typeof window === 'undefined' || !ref.current) return
+		if (sessionStorage.getItem(`hide-${game.gamePk}`) === 'true') {
+			ref.current.checked = true
+		}
+	}, [])
+
 	return (
 		<div className="anim-fade-to-l absolute inset-y-0 right-0 hidden place-content-center gap-2 px-4 group-hover:grid group-focus-visible:grid hide-score:grid">
 			<div className="blur-gradient-to-l absolute inset-0 -left-16 [--amount:50%]" />
 
 			<label className="relative flex gap-2">
 				<input
+					ref={ref}
 					className="hide-score"
 					type="checkbox"
 					hidden
-					defaultChecked={
-						typeof window !== 'undefined' &&
-						sessionStorage.getItem(`hide-${game.gamePk}`) === 'true'
-					}
 					onChange={(e) => {
 						if (typeof window === 'undefined') return
 
